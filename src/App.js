f5972d4fb197fc4276141eb39486adba370d6069
LadPaule <ssozipaule@gmail.com>
2022-02-18 21:12:17 +0300

    styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <h1>building slack</h1>
+      <Router>
+        <>
+          <Routes>
+            <Route path="/" exact element={< Header />} />
+          </Routes>
+        </>
+      </Router>
     </div>
   );
 }
