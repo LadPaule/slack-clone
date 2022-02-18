bc15140b8044e1d15fc96351cc0daa22f28e3d0a
LadPaule <ssozipaule@gmail.com>
2022-02-18 18:58:52 +0300

    setup

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC92THqWI3TjQN2jjiURAo2PEjPUWI_Oho",
+  authDomain: "scu-forum.firebaseapp.com",
+  projectId: "scu-forum",
+  storageBucket: "scu-forum.appspot.com",
+  messagingSenderId: "474948613541",
+  appId: "1:474948613541:web:5b86d6b1ae6c20524d244c",
+};
